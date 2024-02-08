855bc6cab7022cdfef1d16fa03836eb561dd7c43
ivonne alexandra aguilar carrero <ivonneaguilar3@gmail.com>
2024-02-08 18:15:45 -0500

    agregué parte 2 de mi proyecto

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import readchar
+
+
 nombre = input("¡jugador! ¡ingresa tu nickname!: ")
 print("¡Bienveid@ a esta travesía! ",nombre," ¡toma las desiciones correctas!")
+
+
+
+
+while True:
+    char = readchar.readkey()
+    if char == readchar.key.UP:
+        print("Fin del programa")
+        break
+    else:
+        print("Tecla presionada:", char)
